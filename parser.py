bdee958f697d1e2a6117bb2ca30a068250595445
Loren Maggiore <lorenrose1013@aol.com>
2016-03-19 16:52:17 -0400

    finished parser.py

--- parser.py
+++ parser.py
@@ -9,32 +9,72 @@ def parse_file( fname, points, transform, screen, color ):
     while (i < len(file_contents) ):
         line = file_contents[i]
         if ("line" in line):
+        	#add_edge( matrix, x0, y0, z0, x1, y1, z1 ):
             args = file_contents[i+1]
             args = args.split(" ")
             draw.add_edge(points, args[0], args[1], args[2], args[3], args[4], args[5])
             i += 1
         elif("circle" in line):
-            #case circle
+        	#add_circle( points, cx, cy, cz, r, step ):
+        	args = file_contents[i+1]
+            args = args.split(" ")
+            draw.add_circle(points, args[0], args[1], args[2], 0, 1)
+            i+=1
         elif("hermite" in line):
-            #case hermite
+        	#add_curve( points, x0, y0, x1, y1, x2, y2, x3, y3, step, curve_type ):
+        	#for hermit(points, x0, y0, dx0, dy0, x1, y1, dx1, dy1, step, curve_type )
+            args = file_contents[i+1]
+            args = args.split(" ")
+            draw.add_circle(points, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], 1, "hermite")
+            i += 1
         elif("b" in line):
-            #case bezier
+            #add_curve( points, x0, y0, x1, y1, x2, y2, x3, y3, step, curve_type ):
+        	#for bezier(points, x0, y0, x1, y1, x2, y2, x3, y3, step, curve_type )
+            args = file_contents[i+1]
+            args = args.split(" ")
+            draw.add_circle(points, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], 1, "bezier")
+            i += 1
         elif("ident" in line):
-            #case identity
+            matrix.ident(transform)
         elif("scale" in line):
-            #case scale
+        	args = file_contents[i+1]
+            args = args.split(" ")
+            scalar = matrix.make_scale(args[0], args[1], args[2])
+            matrix.matrix_mult(scalar, transform)
+            i += 1
         elif("translate" in line):
-            #case translate
+            args = file_contents[i+1]
+            args = args.split(" ")
+            trans = matrix.make_translate(args[0], args[1], args[2])
+            matrix.matrix_mult(trans, transform)
+            i += 1
         elif("xrotate" in line):
-            #case xrotate       
+            args = file_contents[i+1]
+            args = args.split(" ")
+            xRot = matrix.make_rotX(args[0])
+            matrix.matrix_mult(xRot, transform)
+            i += 1      
         elif("yrotate" in line):
-            #case yrotate       
+            args = file_contents[i+1]
+            args = args.split(" ")
+            yRot = matrix.make_rotY(args[0])
+            matrix.matrix_mult(yRot, transform)
+            i += 1      
         elif("zrotate" in line):
-            #case zrotate       
+            args = file_contents[i+1]
+            args = args.split(" ")
+            zRot = matrix.make_rotZ(args[0])
+            matrix.matrix_mult(zRot, transform)
+            i += 1       
         elif("apply" in line):
-            #case apply       
+            matrix.matrix_mult(transform, points)      
         elif("display" in line):
-            #case display       
+            draw.draw_lines(points, screen, color) 
+            display(screen)    
         elif("save" in line):
-            #case save       
+        	args = file_contents[i+1]
+            args = args.split(" ")
+            draw.draw_lines(points, screen, color)
+            save_ppm(screen, arg[0])
+            i+=1
 
